550d5ecb4a101e30c7c4b2396ff2324bdff97e32
Metal Master <metal.master.g3@gmail.com>
2016-06-22 21:09:29 +0000

    update

--- Uppkendo/app.js
+++ Uppkendo/app.js
@@ -6,12 +6,11 @@
     };
 
     var bootstrap = function() {
-		
         $(function() {
             app.mobileApp = new kendo.mobile.Application(document.body, {
-                //platform:'ios7',
-				layout:"mobile-tabstrip",
-                initial: 'components/homeView/view.html',
+                transition: 'slide',
+                skin: 'flat',
+                initial: 'components/homeView/view.html'
             });
         });
     };
@@ -21,47 +20,15 @@
             if (navigator && navigator.splashscreen) {
                 navigator.splashscreen.hide();
             }
-            
-            var element = document.getElementById('appDrawer');
-            if (typeof(element) != 'undefined' && element !== null) {
-                if (window.navigator.msPointerEnabled) {
-                    $('#navigation-container').on('MSPointerDown', 'a', function(event) {
-                        app.keepActiveState($(this));
-                    });
-                } else {
-                    $('#navigation-container').on('touchstart', 'a', function(event) {
-                        app.keepActiveState($(this));
-                    });
-                }
-            }
-            //console.log(cordova.file); //esta linea evita que la pantalla se quede en blanco cuando la app esta firmada
-            
             bootstrap();
         }, false);
     } else {
         bootstrap();
     }
-    
-    
-    if (kendo.support.mobileOS.android) {
-        $(document).on({
-            ajaxStart: function() {
-                //alert('Inicio');
-                var $div = $('<div id = "cargando" class="modall"></div>');
-                $("body").append($div);
-                $("#cargando").show().css('display', 'block');
-            },
-            ajaxStop: function() {
-                $("#cargando").show().css('display', 'none');
-                $('#cargando').remove();
-                //alert('Fin');
-            }
-        });
-    }
-	
+
     app.keepActiveState = function _keepActiveState(item) {
         var currentItem = item;
-        $('#navigation-container li a.active').removeClass('active');
+        $('#navigation-container li.active').removeClass('active');
         currentItem.addClass('active');
     };
 
@@ -74,9 +41,21 @@
             return navigator.connection.type !== 'none';
         }
     };
+
+    app.openLink = function(url) {
+        if (url.substring(0, 4) === 'geo:' && device.platform === 'iOS') {
+            url = 'http://maps.apple.com/?ll=' + url.substring(4, url.length);
+        }
+
+        window.open(url, '_system');
+        if (window.event) {
+            window.event.preventDefault && window.event.preventDefault();
+            window.event.returnValue = false;
+        }
+    };
+
 }());
 
 // START_CUSTOM_CODE_uppkendo
-
 
 // END_CUSTOM_CODE_uppkendo
